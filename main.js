80152c256022e4fa6aca49e3090bb235455404de
Baskars1234 <76224896+Baskars1234@users.noreply.github.com>
2021-08-24 17:52:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/DmOrS2yNC/', modelReady());
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
